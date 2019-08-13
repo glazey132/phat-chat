fa97d57c1346b43e29ee1651a5a76d84a02f92c2
glazey132 <alglaze132@gmail.com>
2019-08-13 15:58:09 -0700

    initial commit after segmenting client and server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input
+} from "reactstrap";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Header />
+
+      <Route exact path="/" component={Login} />
+      <Route path="/register" component={Register} />
+        {/* <Route path="/topics" component={Topics} /> */}
+    </Router>
+  );
+}
+
+function Login() {
+  return (
+    <Container fluid className="auth-container">
+      <Row>
+        <Col>
+            <h1>Login</h1>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+              <Input placeholder="username" />
+            </InputGroup>
+            <br />
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+              <Input placeholder="password" />
+            </InputGroup>
+            <a href="/register">Sign Up</a>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+function Register() {
+  return (
+    <Container fluid className="auth-container">
+      <Row>
+        <Col>
+            <h1>Register</h1>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+              <Input placeholder="email" />
+            </InputGroup>
+            <br />
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+              <Input placeholder="Password" />
+            </InputGroup>
+            <br />
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+              <Input placeholder="Confirm Password" />
+            </InputGroup>
+            <a href="/">Login</a>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
     </div>
   );
 }
